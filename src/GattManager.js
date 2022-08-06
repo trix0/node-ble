0bd666e60446ebe61d1ed97e0aa42831b899e0e5
Matúš Šatara <mas@o2matic.com>
2022-08-06 17:39:42 +0200

    back

--- src/GattManager.js
+++ src/GattManager.js
@@ -7,12 +7,12 @@ const BusHelper = require('./BusHelper')
 class GattManager {
   constructor (dbus) {
     this.dbus = dbus
-    this.helper = new BusHelper(dbus, 'org.bluez', '/org/bluez/hci0', 'org.freedesktop.DBus.ObjectManager')
+    this.helper = new BusHelper(dbus, 'org.bluez', '/org/bluez/hci0', 'org.bluez.GattManager1')
     this.init()
   }
 
   async init () {
-    const managedObjects = await this.helper.prop("org.bluez.GattManager1")
+    const managedObjects = await this.helper.props()
     console.log(JSON.stringify(managedObjects));
     managedObjects.forEach(element => {
         console.log(JSON.stringify(element));
